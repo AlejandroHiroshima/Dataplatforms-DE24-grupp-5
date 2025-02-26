248a62c525a5c5a0682a8e4c20c2c7e1372c16a0
964Code <964code@gmail.com>
2025-02-26 20:35:37 +0100

    bad import, fixed,

--- src/dashboards/resources/charts.py
+++ src/dashboards/resources/charts.py
@@ -1,12 +1,14 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
+
 def line_chart(x, y, **options):
     fig, ax = plt.subplots(1)
 
     ax.plot(x, y, linewidth=4)
 
     ax.set(**options)
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Price')
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Price")
     plt.xticks(rotation=45)
     fig.tight_layout()
     return fig
